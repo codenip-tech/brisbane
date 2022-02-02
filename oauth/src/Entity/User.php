7faba550a69bfc0af3decd8ffde9beca7a7fb2a7
Moein Akbarof <moein@tropee.co>
2022-02-02 21:47:04 +0100

    feature-24/add-user-for-oauth-server
    * Added user for oauth server
    
    Fixes #24

---
+++ oauth/src/Entity/User.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'NONE')]
+    #[ORM\Column(type: 'string', columnDefinition: 'CHAR(36) NOT NULL')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $firstName;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $lastName;
+
+    public function __construct(
+        string $id,
+        string $firstName,
+        string $lastName,
+    )
+    {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+}
