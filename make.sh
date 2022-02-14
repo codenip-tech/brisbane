b2cdc33419e810de5cbf040b12bcd7497bd1e311
lologalvez <48884601+lologalvez@users.noreply.github.com>
2022-02-14 21:00:56 +0100

    feature/33-unify-makefile-for-app-and-oauth (#40)
    * Add unified Makefile in root and include it in both app/Makefile and oauth/Makefile
    * Add unified make.sh in root and add symlinks to each project
    
    Fixes #33

---
+++ make.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+export U_ID=$(id -u)
+COMMAND=$1
+DOCKER_BE=$2
+
+case ${COMMAND} in
+  ssh-be)
+    docker exec -it --user "${U_ID}" "${DOCKER_BE}" bash
+    ;;
+  start)
+    docker network create brisbane-network 2> /dev/null || true
+    U_ID=${UID} docker-compose up -d
+    ;;
+  stop)
+    U_ID=${UID} docker-compose stop
+    ;;
+  build)
+	  cp -n docker-compose.yml.dist docker-compose.yml || true
+    docker network create brisbane-network 2> /dev/null || true
+    U_ID=${UID} docker-compose build
+    ;;
+  code-style)
+    docker exec --user ${UID} ${DOCKER_BE} php-cs-fixer fix src --rules=@Symfony
+    ;;
+  code-style-check)
+    docker exec --user ${UID} ${DOCKER_BE} php-cs-fixer fix src --rules=@Symfony --dry-run
+    ;;
+  run)
+    U_ID=${UID} docker exec -it --user ${UID} ${DOCKER_BE} symfony serve -d
+    ;;
+  logs)
+    U_ID=${UID} docker exec -it --user ""${UID} ${DOCKER_BE} symfony server:log
+    ;;
+  composer-install)
+    U_ID=${UID} docker exec --user ${UID} ${DOCKER_BE} composer install --no-interaction
+    ;;
+  tests)
+	  U_ID=${UID} docker exec --user ${UID} ${DOCKER_BE} vendor/bin/simple-phpunit -c phpunit.xml.dist 2> /dev/null || true
+    ;;
+  generate-private-key)
+    openssl genrsa -out private.key 2048
+    ;;
+  *)
+    echo "Command ${COMMAND} not implemented"
+    ;;
+esac
