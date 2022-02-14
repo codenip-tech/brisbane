88f16453638088d2353d500d0e4fae6bf6e17dc0
Rafael de la Escalera <45367402+escalerarodriguezr@users.noreply.github.com>
2022-02-14 16:11:22 +0100

    feature/38-add-email-and-password-field-for-user (#48)
    * feature/38-add-email-and-password-field-for-user
    
    * review-feature/38-add-email-and-password-field-for-user
    
    Fix #38

--- oauth/src/Entity/User.php
+++ oauth/src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[ORM\UniqueConstraint(name: "U_user_email", columns: ["email"])]
 class User
 {
     #[ORM\Id]
@@ -20,15 +21,24 @@ class User
     #[ORM\Column(type: 'string', length: 50)]
     private $lastName;
 
+    #[ORM\Column(type: 'string', length: 100)]
+    private string $email;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private ?string $password;
+
     public function __construct(
         string $id,
         string $firstName,
         string $lastName,
+        string $email,
     )
     {
         $this->id = $id;
         $this->firstName = $firstName;
         $this->lastName = $lastName;
+        $this->setEmail($email);
+        $this->password = null;
     }
 
     public function getId(): ?int
@@ -59,4 +69,29 @@ class User
 
         return $this;
     }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        if (!\filter_var($email, \FILTER_VALIDATE_EMAIL)) {
+            throw new \LogicException('Invalid email');
+        }
+        $this->email = $email;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+
 }
