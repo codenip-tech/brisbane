83b62f5325be85e4ff64d6da24b5f8b46068bc3e
Rafa Díaz <diaz.garcia.rafa@gmail.com>
2022-05-09 16:19:43 +0200

    fix/71-fix-confirm-password-field-security-in-register-form (#78)
    - Change password and confirm password fields in register form
    - Change password type in UserType class to validate the use of the same password in both fields
    
    Fix #71

--- oauth/src/Form/UserType.php
+++ oauth/src/Form/UserType.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,7 +21,12 @@ class UserType extends AbstractType
             ->add('firstName')
             ->add('lastName')
             ->add('email', EmailType::class)
-            ->add('password', PasswordType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password'],
+            ])
             ->add('save', SubmitType::class);
     }
 
