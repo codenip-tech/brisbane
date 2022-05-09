91bfa3d8be4b74e2d7cf33c0a3c05255e10549cb
Rafa Díaz <diaz.garcia.rafa@gmail.com>
2022-05-09 16:25:06 +0200

    fix/68-create-private-key-with-right-permissions (#75)
    * fix/68-create-private-key-with-right-permissions
    
    - Create private.key file with 644 permissions when execute make generate-private-key
    
    * fix/68-create-private-key-with-right-permissions
    
    - Add command to generate public key
    
    * fix/68-create-private-key-with-right-permissions
    
    - Create the OAUTH_KEYS_DIR variable to store the path where the keys will be stored (brisbane/var/oauth)
    regardless of which Makefile is used (brisbane/Makefile or brisbane/oauth/Makefile).
    
    - Create one make function to generate the private key, give it the
    right permissions, generate the public key and move them to brisbane/var/oauth directory.
    
    * fix/68-create-private-key-with-right-permissions
    
    - Fix keys folder location
    
    Fix #68

--- make.sh
+++ make.sh
@@ -3,6 +3,7 @@ set -e
 export U_ID=$(id -u)
 COMMAND=$1
 DOCKER_BE=$2
+OAUTH_KEYS_DIR="$(dirname "$(readlink -f "$0")")"/oauth/var/oauth
 
 case ${COMMAND} in
   ssh-be)
@@ -41,8 +42,13 @@ case ${COMMAND} in
   tests)
 	  U_ID=${UID} docker exec --user ${UID} ${DOCKER_BE} vendor/bin/simple-phpunit -c phpunit.xml.dist 2> /dev/null || true
     ;;
-  generate-private-key)
+  generate-oauth-keys)
     openssl genrsa -out private.key 2048
+    chmod 644 private.key
+    openssl rsa -in private.key -pubout -out public.key
+    mkdir -p "${OAUTH_KEYS_DIR}"
+    mv private.key "${OAUTH_KEYS_DIR}"/private.key
+    mv public.key "${OAUTH_KEYS_DIR}"/public.key
     ;;
   *)
     echo "Command ${COMMAND} not implemented"
